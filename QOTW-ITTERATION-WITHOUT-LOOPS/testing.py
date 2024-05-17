904d40e73c5d5b5664b47a78d5e9bae965d6d48d
Henry112q <griffithseuan01@icloud.com>
2024-05-17 11:04:40 +0100

    Tests

---
+++ QOTW-ITTERATION-WITHOUT-LOOPS/testing.py
@@ -0,0 +1,3 @@
+print("hello world") 
+for x in range(0,10):
+  print(x)
--- QOTW-ITTERATION-WITHOUT-LOOPS/testingOS.py
+++ QOTW-ITTERATION-WITHOUT-LOOPS/testingOS.py
@@ -1,4 +1,9 @@
 import os
+code = 'print("hello world") \nfor x in range(0,10):\n  print(x)'
 
-os.system("python.exe")
-os.system("print('hi')")
+a = open(r"QOTW-ITTERATION-WITHOUT-LOOPS/testing.py",'w')
+a.write(code)
+a.close()
+
+os.system(r"python.exe H:\QOTW-CODE-PROOF\QOTW-ITTERATION-WITHOUT-LOOPS\testing.py")
+
