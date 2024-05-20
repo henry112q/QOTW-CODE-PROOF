0cafa57b59edfa4af632ecd17bfaa9c8c4b9d2d5
Henry112q <griffithseuan01@icloud.com>
2024-05-20 13:46:08 +0100

    sdaodsadh

--- QOTW-ITTERATION-WITHOUT-LOOPS/testing.py
+++ QOTW-ITTERATION-WITHOUT-LOOPS/testing.py
@@ -1,3 +1,6 @@
-print("hello world") 
-for x in range(0,10):
-  print(x)
+import os 
+code = "for x in range(0,10): print(x)"
+a=open("H:/QOTW-CODE-PROOF/QOTW-ITTERATION-WITHOUT-LOOPS/test1.py","w") 
+a.write(code) 
+a.close()
+os.system("python.exe H:QOTW-CODE-PROOF/QOTW-ITTERATION-WITHOUT-LOOPS/test1.py")
--- QOTW-ITTERATION-WITHOUT-LOOPS/testingOS.py
+++ QOTW-ITTERATION-WITHOUT-LOOPS/testingOS.py
@@ -1,9 +1,8 @@
 import os
-code = 'print("hello world") \nfor x in range(0,10):\n  print(x)'
+code = 'import os \ncode = "for x in range(0,10): print(x)"\na=open("H:/QOTW-CODE-PROOF/QOTW-ITTERATION-WITHOUT-LOOPS/test1.py","w") \na.write(code) \na.close()\nos.system("python.exe H:QOTW-CODE-PROOF/QOTW-ITTERATION-WITHOUT-LOOPS/test1.py")'
 
-a = open(r"QOTW-ITTERATION-WITHOUT-LOOPS/testing.py",'w')
+a = open(r"H:\QOTW-CODE-PROOF\QOTW-ITTERATION-WITHOUT-LOOPS\testing.py",'w')
 a.write(code)
 a.close()
-
-os.system(r"python.exe H:\QOTW-CODE-PROOF\QOTW-ITTERATION-WITHOUT-LOOPS\testing.py")
-
+for x in range(4):
+    os.system(r"python.exe H:\QOTW-CODE-PROOF\QOTW-ITTERATION-WITHOUT-LOOPS\testing.py")
